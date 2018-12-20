299ca094211de7df0744c1c2ff5737b8094bfd8c
Konstantin Tsolev <ktsolev@gmail.com>
2018-12-20 12:45:28 +0200

    Added firebase sdk to the project

--- android/app/src/main/java/com/tabeksapp/MainApplication.java
+++ android/app/src/main/java/com/tabeksapp/MainApplication.java
@@ -2,6 +2,8 @@ package com.tabeksapp;
 
 import android.app.Application;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -11,7 +13,9 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.github.yamill.orientation.OrientationPackage;
-
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,11 +29,15 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
+            new MainReactPackage(),
+            new RNFirebasePackage(),
             new MapsPackage(),
             new ReactNativePushNotificationPackage(),
             new LinearGradientPackage(),
-            new OrientationPackage()
+            new OrientationPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseMessagingPackage(),
+            new RNFirebaseNotificationsPackage()
       );
     }
 
@@ -50,4 +58,4 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
   }
 
-} 
+}
